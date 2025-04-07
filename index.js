7cee966bc05979a32a9ab6cb188f29a5214b895d
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-04-07 23:38:56 +0300

    practised using fetch

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(Response => Response.json())
+  .then(data => {
+   renderBooks(data)
+   //console.log(data);
+   return data;
+  });
 }
 
 function renderBooks(books) {
